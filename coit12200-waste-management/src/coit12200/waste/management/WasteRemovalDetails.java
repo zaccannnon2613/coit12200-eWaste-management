a2b8d9839a6897fcf3fc03825af92b01410af7ca
12195928 <12195928@192.168.56.1>
2023-05-26 13:00:16 +1000

    waste details started

---
+++ coit12200-waste-management/src/coit12200/waste/management/WasteRemovalDetails.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coit12200.waste.management;
+
+/**
+ *
+ * @author 12195928
+ */
+public class WasteRemovalDetails {
+    private String typeOfRemoval;
+    private String individualItems;
+    private int quantityOfItems;
+    private String requestDate;
+    private String timeOfRemoval;
+    private String locationDetails;
+    private String notes;
+    
+}
